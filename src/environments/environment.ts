66b072b8acd793e6456b4c83338a511cbde0ea9b
Sandro <blacksatiuz@gmx.de>
2023-02-09 20:52:39 +0100

    feat: change base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/api',
+  baseUrl: 'https://seminarws22frontend-production.up.railway.app/',
+  //baseUrl: 'http://localhost:3000/api',
 };
