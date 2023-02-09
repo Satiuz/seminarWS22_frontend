9d2742e29f3d3ca50c65efba1d6ec8e294a18840
Sandro <sandro@uni-kassel.de>
2023-02-09 13:47:05 +0100

    feat: add server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/dist/seminar-w22-frontend'));
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+
+const server = http.createServer(app);
+
+
+/*
+server.listen(port, () => {
+    console.log(`Process Env dirname${process.env.__dirname} | Dirname : ${__dirname}`);
+    console.log(`App running on: https://seminarws22-frontend-maiswaffeln.onrender.com:${port}`);
+});
+*/
