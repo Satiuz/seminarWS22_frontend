1e691a6a2104ebad2590842e2a9f7c54ea73dd17
Sandro <uk061779@student.uni-kassel.de>
2023-03-23 14:16:35 +0100

    fix: change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'seminarws22backend-production-2869.up.railway.app/api',
-  //baseUrl: 'https://seminarws22backend-production.up.railway.app/api', // TODO
+  baseUrl: 'https://seminarws22backend-production.up.railway.app/api', // TODO
 };
